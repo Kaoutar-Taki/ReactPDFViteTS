6fc53230b8ed26bc9793056e10b4860d21a2489f
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-03-21 11:49:21 +0000

    add data in file extern

---
+++ testpdf/src/data/Donner.ts
@@ -0,0 +1,284 @@
+const donnees = [
+    {
+        Désignation: "xxxxxxxxxxxxx => xxxxxxxxxxxxxxxxxxxxxxxxx",
+        Long: null,
+        Larg: 2.40,
+        Ep: 1.15,
+        Pce: 0.02,
+        Métrage: "41.40 M2",
+        PU: "200.00",
+        "HT Total": "8,280.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxx => xxxxxxxxxxxxxxxxxxxxxxxxx",
+        Long: null,
+        Larg: 2.00,
+        Ep: 1.05,
+        Pce: 0.02,
+        Métrage: "27.30 M2",
+        PU: "100.00",
+        "HT Total": "2,730.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxx => xxxxxxxxxxxxxxxxxxxxxxxxx",
+        Long: null,
+        Larg: 2.35,
+        Ep: 1.25,
+        Pce: 0.02,
+        Métrage: "32.31 M2",
+        PU: "100.00",
+        "HT Total": "3,231.25"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+    {
+        Désignation: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx => xxxxxxxxxxxx",
+        Long: 1.00,
+        Larg: 1.00,
+        Ep: 0.02,
+        Pce: 33,
+        Métrage: "33.00 M2",
+        PU: "250.00",
+        "HT Total": "8,250.00"
+    },
+];
+
+export default donnees;
